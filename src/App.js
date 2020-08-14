701085760161a704c2765680cef9e272dcc4dc9a
itsdaffa <daffa9700@gmail.com>
2020-08-14 16:30:43 +0700

    Akhirnya bisa lengkap :)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./components/routes";
 import {AuthProvider} from './components/authcontext'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProtectedRoute from "./components/protectedroute";
+
 export default function App() {
   return (
     <AuthProvider>
-    <>
       <Router>
         <Routes />
       </Router>
-    </>
     </AuthProvider>
   );
 }
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,5 +1,7 @@
 import React, {useState, useContext} from 'react'
 import {Switch, Route} from "react-router"
+import {useHistory} from 'react-router-dom'
+import history from './history'
 
 import {Home, Navbar} from '../main/index'
 import About from '../main/about'
@@ -9,21 +11,20 @@ import Login from '../main/login'
 
 import ProtectedRoute from './protectedroute'
 
-import {AuthProvider, AuthContext} from './authcontext'
+import {AuthContext} from './authcontext'
 
 const Routes = () => {
   const [user, setUser] = useContext(AuthContext)
-  console.log(useContext(AuthContext  ))
+  // const history = useHistory()
+  // console.log(useContext(AuthContext))
 
-  const handleLogin = e => {
-    e.preventDefault()
-    setUser(true)
-  }
+  // const handleLogin = e => {
+  //   e.preventDefault()
+  //   history.push("/about")
+  //   setUser(true)
+  // }
 
-  const handleLogout = e => {
-    e.preventDefault()
-    setUser(false)
-  }
+
   return (
 
   <>
@@ -32,10 +33,12 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path = "/contact" component={Contact} />
-      <Route exact path = "/login" 
-        render={props => <Login {...props} user={user} handleLogin={handleLogin} />} />
-      <Route exact path ="/editor"
-        render = {props => <Editor {...props} user={user} handleLogout={handleLogout}/>}/>
+      <Route exact path = "/login">
+        <Login />
+      </Route>
+      {/* <Route exact path = "/login" 
+        render={props => <Login {...props} user={user} handleLogin={handleLogin} />} /> */}
+      <ProtectedRoute exact path ="/editor" user={user} component={Editor}/>
     </Switch>
 </> 
   
