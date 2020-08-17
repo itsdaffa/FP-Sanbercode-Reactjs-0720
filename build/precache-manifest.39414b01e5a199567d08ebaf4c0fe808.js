44a2c56a3af5f1985463c152fad266855f9bf335
itsdaffa <daffa9700@gmail.com>
2020-08-17 19:04:14 +0700

    upload build

---
+++ build/precache-manifest.39414b01e5a199567d08ebaf4c0fe808.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cb2864b63fa91c9734f7a3afe07313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b441c4e42cd5ddd29e16",
+    "url": "/static/css/2.a1f2881c.chunk.css"
+  },
+  {
+    "revision": "f000dc9e2801d7aa53bd",
+    "url": "/static/css/main.b21f1d22.chunk.css"
+  },
+  {
+    "revision": "b441c4e42cd5ddd29e16",
+    "url": "/static/js/2.3aef2af1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3aef2af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f000dc9e2801d7aa53bd",
+    "url": "/static/js/main.7bb7a774.chunk.js"
+  },
+  {
+    "revision": "f773a68b909643a98b65",
+    "url": "/static/js/runtime-main.361c84da.js"
+  },
+  {
+    "revision": "b59badc3df50034e5326cf4ff10e5e58",
+    "url": "/static/media/pattern.b59badc3.jpg"
+  }
+]);
